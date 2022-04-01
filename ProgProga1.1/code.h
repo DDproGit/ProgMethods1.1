14b76d171d83b55fc7f31980b3742379d673a52a
NSigarev <nikita.sigarev@mail.ru>
2022-04-02 01:26:37 +0700

    Delete some useless comments

--- ProgProga1.1/code.h
+++ ProgProga1.1/code.h
@@ -23,26 +23,6 @@ struct Shape1
 	};
 };
 
-//class Shape
-//{
-//public:
-//	Shape();
-//	Shape(int radius);
-//	Shape(int heigth, int width, int depth);
-//	~Shape();
-//	void setValues(int radius);
-//	void setValues(int heigth, int width, int depth);
-//	int getRadius();
-//	int getHeigth();
-//	int getWidth();
-//	int getDepth();
-//protected:
-//	int radius;
-//	int heigth;
-//	int width;
-//	int depth;
-//private:
-//};
 
 struct HashArray1
 {
@@ -60,23 +40,3 @@ Shape1 getElement(int hash, int place, HashArray1& myArray);
 int getSizeOfVector(int hash, HashArray1& myArray);
 // Return hash of element
 int makeHashOfShape(Shape1 shapeToHash);
-
-//class HashArray
-//{
-//public:
-//	// Adds element to array and return hash and place
-//	std::pair <int, int> addElement(Shape newElement);
-//	// Delete element from array by hash and place
-//	void removeElement(int hash, int place);
-//	// Replace element with this hash and place with new 
-//	void replaceElement(int hash, int place, Shape newShape);
-//	// Return element by hash and place
-//	Shape getElement(int hash, int place);
-//	int getSizeOfVector(int hash);
-//	// Return hash of element
-//	int makeHashOfShape(Shape shapeToHash);
-//	std::pair <int, int> findElement();
-//protected:
-//	std::vector<Shape> arrayOfVectorsOfElements[30];
-//private:
-//};
