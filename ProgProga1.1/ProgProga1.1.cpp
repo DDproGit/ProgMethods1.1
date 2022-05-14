78a2a9a08dfea271c8c5679dd07881c23e58ff13
NSigarev <nikita.sigarev@mail.ru>
2022-05-14 13:14:43 +0700

    safed

--- ProgProga1.1/ProgProga1.1.cpp
+++ ProgProga1.1/ProgProga1.1.cpp
@@ -2,6 +2,9 @@
 #include "code.h"
 #include <vector>
 #include <fstream>
+#include <string> 
+#include <fstream>
+#include <boost/algorithm/string.hpp>
 
 using namespace std;
 
@@ -30,69 +33,100 @@ int main(int argc, char* argv[])
     }
     int n = 0;
     in >> n;
+    std::string tmpstr;
+    std::getline(in, tmpstr);
     for (int i = 0; i < n; i++)
     {
         int type, radius, heigth, width, depth, temperature, edge = 0;
         float density = 0.00;
-        in >> type;
-        switch(type)
+        std::string inputline;
+        std::getline(in, inputline);
+        std::vector<std::string> split_vector;
+        boost::split(split_vector, inputline, boost::is_any_of(" "), boost::token_compress_on);
+        split_vector.erase(
+            std::remove_if(split_vector.begin(), split_vector.end(),
+                [](std::string const& s) { return s.size() == 0; }), split_vector.end());
+        switch(stoi(split_vector[0]))
         {
             case Shape1::type::round:
             {
-                Shape1 tmp;
-                in >> density;
-                in >> temperature;
-                in >> radius;
-                tmp.key = Shape1::type::round;
-                tmp.mySphere.radius = radius;
-                tmp.mySphere.temperature = temperature;
-                tmp.mySphere.density = density;
-                myElement = addElement(tmp, array);
-                std::cout << "Element number " << i << " is a sphere with radius: ";
-                std::cout << tmp.mySphere.radius << ", temperature ";
-                std::cout << tmp.mySphere.temperature << ", density ";
-                std::cout << tmp.mySphere.density << ", volume ";
-                std::cout << tmp.mySphere.getVolume() << "\n";
+                if (split_vector.size() == 4)
+                {
+                    Shape1 tmp;
+                    density = stof(split_vector[1]);
+                    temperature = stoi(split_vector[2]);
+                    radius = stoi(split_vector[3]);
+                    tmp.key = Shape1::type::round;
+                    tmp.mySphere.radius = radius;
+                    tmp.mySphere.temperature = temperature;
+                    tmp.mySphere.density = density;
+                    myElement = addElement(tmp, array);
+                    std::cout << "Element number " << i << " is a sphere with radius: ";
+                    std::cout << tmp.mySphere.radius << ", temperature ";
+                    std::cout << tmp.mySphere.temperature << ", density ";
+                    std::cout << tmp.mySphere.density << ", volume ";
+                    std::cout << tmp.mySphere.getVolume() << "\n";
+                }
+                else
+                {
+                    std::cout << "Incorrect number of arguments \n";
+                }
                 break;
             }
             case Shape1::type::square:
             {
-                Shape1 tmp;
-                in >> density;
-                in >> temperature;
-                in >> heigth >> width >> depth;
-                tmp.key = Shape1::type::square;
-                tmp.myParallelepiped.heigth = heigth;
-                tmp.myParallelepiped.width = width;
-                tmp.myParallelepiped.depth = depth;
-                tmp.myParallelepiped.temperature = temperature;
-                tmp.myParallelepiped.density = density;
-                myElement = addElement(tmp, array);
-                std::cout << "Element number " << i << " is a parallelepiped with edges: ";
-                std::cout << tmp.myParallelepiped.heigth << ", ";
-                std::cout << tmp.myParallelepiped.width << ", ";
-                std::cout << tmp.myParallelepiped.depth << ", temperature ";
-                std::cout << tmp.myParallelepiped.temperature << ", density ";
-                std::cout << tmp.myParallelepiped.density << ", volume ";
-                std::cout << tmp.myParallelepiped.getVolume() << "\n";
+                if (split_vector.size() == 6)
+                {
+                    Shape1 tmp;
+                    density = stof(split_vector[1]);
+                    temperature = stoi(split_vector[2]);
+                    heigth = stoi(split_vector[3]);
+                    width = stoi(split_vector[4]);
+                    depth = stoi(split_vector[5]);
+                    tmp.key = Shape1::type::square;
+                    tmp.myParallelepiped.heigth = heigth;
+                    tmp.myParallelepiped.width = width;
+                    tmp.myParallelepiped.depth = depth;
+                    tmp.myParallelepiped.temperature = temperature;
+                    tmp.myParallelepiped.density = density;
+                    myElement = addElement(tmp, array);
+                    std::cout << "Element number " << i << " is a parallelepiped with edges: ";
+                    std::cout << tmp.myParallelepiped.heigth << ", ";
+                    std::cout << tmp.myParallelepiped.width << ", ";
+                    std::cout << tmp.myParallelepiped.depth << ", temperature ";
+                    std::cout << tmp.myParallelepiped.temperature << ", density ";
+                    std::cout << tmp.myParallelepiped.density << ", volume ";
+                    std::cout << tmp.myParallelepiped.getVolume() << "\n";
+                }
+                else
+                {
+                    std::cout << "Incorrect number of arguments \n";
+                }
                 break;
             }
             case Shape1::type::tetra:
             {
-                Shape1 tmp;
-                in >> density;
-                in >> temperature;
-                in >> edge;
-                tmp.key = Shape1::type::tetra;
-                tmp.myTetraedr.edge = edge;
-                tmp.myTetraedr.temperature = temperature;
-                tmp.myTetraedr.density = density;
-                myElement = addElement(tmp, array);
-                std::cout << "Element number " << i << " is a tetraedr with edge: ";
-                std::cout << tmp.myTetraedr.edge << ", temperature ";
-                std::cout << tmp.myTetraedr.temperature << ", density ";
-                std::cout << tmp.myTetraedr.density << ", volume ";
-                std::cout << tmp.myTetraedr.getVolume() << "\n";
+                if (split_vector.size() == 4)
+                {
+                    Shape1 tmp;
+                    density = stof(split_vector[1]);
+                    temperature = stoi(split_vector[2]);
+                    heigth = stoi(split_vector[3]);
+                    tmp.key = Shape1::type::tetra;
+                    tmp.myTetraedr.edge = edge;
+                    tmp.myTetraedr.temperature = temperature;
+                    tmp.myTetraedr.density = density;
+                    myElement = addElement(tmp, array);
+                    std::cout << "Element number " << i << " is a tetraedr with edge: ";
+                    std::cout << tmp.myTetraedr.edge << ", temperature ";
+                    std::cout << tmp.myTetraedr.temperature << ", density ";
+                    std::cout << tmp.myTetraedr.density << ", volume ";
+                    std::cout << tmp.myTetraedr.getVolume() << "\n";
+                }
+                else
+                {
+                    std::cout << "Incorrect number of arguments \n";
+                }
                 break;
             }
             case Shape1::type::empty:
@@ -106,6 +140,11 @@ int main(int argc, char* argv[])
                 tmp.myParallelepiped.temperature = 0;
                 tmp.myParallelepiped.density = 0.00;
                 myElement = addElement(tmp, array);
+                break;
+            }
+            default:
+            {
+                std::cout << "Wrong type of shape\n";
                 break;
             }
         }
